88b206c6776ea63de09614de6b78bbb4b49f7fac
Smahrt Morgan <msmahrt@gmail.com>
2017-02-20 09:39:58 +0100

    Base Files
    New files

---
+++ web/dashboard.php
@@ -0,0 +1,180 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>LAGOS YELLOW - ADMIN</title>
+
+		<!-- BEGIN META -->
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta name="keywords" content="">
+		<meta name="description" content="">
+		<!-- END META -->
+
+		<!-- BEGIN STYLESHEETS -->
+		<link href='http://fonts.googleapis.com/css?family=Roboto:300italic,400italic,300,400,500,700,900' rel='stylesheet' type='text/css'/>
+		<link type="text/css" rel="stylesheet" href="../assets/css/theme/bootstrap.css?1422792965" />
+		<link type="text/css" rel="stylesheet" href="../assets/css/theme/materialadmin.css?1425466319" />
+		<link type="text/css" rel="stylesheet" href="../assets/css/theme/font-awesome.min.css?1422529194" />
+		<link type="text/css" rel="stylesheet" href="../assets/css/theme/material-design-iconic-font.min.css?1421434286" />
+		<!-- END STYLESHEETS -->
+
+		<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+		<!--[if lt IE 9]>
+		<script type="text/javascript" src="../assets/js/libs/utils/html5shiv.js?1403934957"></script>
+		<script type="text/javascript" src="../assets/js/libs/utils/respond.min.js?1403934956"></script>
+		<![endif]-->
+	</head>
+	<body class="header-fixed">
+
+		<!-- BEGIN HEADER-->
+		<header id="header" >
+			<div class="headerbar">
+				<!-- Brand and toggle get grouped for better mobile display -->
+                <div class="headerbar-left">
+					<ul class="header-nav header-nav-options">
+						<li>
+							<a class="btn btn-icon-toggle" data-toggle="offcanvas" href="#navbar">
+								<i class="fa fa-bars"></i>
+							</a>
+						</li>
+						<li class="header-nav-brand">
+							<div class="brand-holder">
+								<a href="../index.php">
+									<span class="text-lg text-bold text-accent">LAGOS YELLOW</span>
+								</a>
+							</div>
+						</li>
+					</ul>
+				</div>
+				<!-- Collect the nav links, forms, and other content for toggling -->
+				<div class="headerbar-right">
+					<ul class="header-nav header-nav-options">
+                        
+					</ul><!--end .header-nav-options -->
+				</div><!--end #header-navbar-collapse -->
+			</div>
+		</header>
+		<!-- END HEADER-->
+
+		<!-- BEGIN BASE-->
+		<div id="base">
+
+			<!-- BEGIN OFFCANVAS LEFT -->
+			<div class="offcanvas">
+                
+                <!-- BEGIN MENUBAR-->
+                <div id="navbar" class="offcanvas-pane brand-primary width-8">
+                    <div class="offcanvas-body">
+                        <!-- BEGIN MAIN MENU -->
+                        <ul id="main-menu" class="gui-controls">
+
+                            <!-- BEGIN DASHBOARD -->
+                            <li>
+                                <a href="../html/dashboards/dashboard.html" >
+                                    <div class="gui-icon"><i class="md md-home"></i></div>
+                                    <span class="title">Dashboard</span>
+                                </a>
+                            </li><!--end /menu-li -->
+                            <!-- END DASHBOARD -->
+
+                            <!-- BEGIN LEVELS -->
+                            <li class="gui-folder">
+                                <a>
+                                    <div class="gui-icon"><i class="fa fa-folder-open fa-fw"></i></div>
+                                    <span class="title">Menu levels demo</span>
+                                </a>
+                                <!--start submenu -->
+                                <ul>
+                                    <li><a href="#"><span class="title">Item 1</span></a></li>
+                                    <li><a href="#"><span class="title">Item 1</span></a></li>
+                                    <li class="gui-folder">
+                                        <a href="javascript:void(0);">
+                                            <span class="title">Open level 2</span>
+                                        </a>
+                                        <!--start submenu -->
+                                        <ul>
+                                            <li><a href="#"><span class="title">Item 2</span></a></li>
+                                            <li class="gui-folder">
+                                                <a href="javascript:void(0);">
+                                                    <span class="title">Open level 3</span>
+                                                </a>
+                                                <!--start submenu -->
+                                                <ul>
+                                                    <li><a href="#"><span class="title">Item 3</span></a></li>
+                                                    <li><a href="#"><span class="title">Item 3</span></a></li>
+                                                    <li class="gui-folder">
+                                                        <a href="javascript:void(0);">
+                                                            <span class="title">Open level 4</span>
+                                                        </a>
+                                                        <!--start submenu -->
+                                                        <ul>
+                                                            <li><a href="#"><span class="title">Item 4</span></a></li>
+                                                            <li class="gui-folder">
+                                                                <a href="javascript:void(0);">
+                                                                    <span class="title">Open level 5</span>
+                                                                </a>
+                                                                <!--start submenu -->
+                                                                <ul>
+                                                                    <li><a href="#"><span class="title">Item 5</span></a></li>
+                                                                    <li><a href="#"><span class="title">Item 5</span></a></li>
+                                                                </ul><!--end /submenu -->
+                                                            </li><!--end /submenu-li -->
+                                                        </ul><!--end /submenu -->
+                                                    </li><!--end /submenu-li -->
+                                                </ul><!--end /submenu -->
+                                            </li><!--end /submenu-li -->
+                                        </ul><!--end /submenu -->
+                                    </li><!--end /submenu-li -->
+                                </ul><!--end /submenu -->
+                            </li><!--end /menu-li -->
+                            <!-- END LEVELS -->
+
+                        </ul><!--end .main-menu -->
+                        <!-- END MAIN MENU -->
+
+                        <div>
+                            <small class="no-linebreak hidden-folded">
+                                <span class="opacity-75">Powered by </span> <strong>Cedarview</strong>
+                            </small>
+                        </div>
+                    </div>
+                </div><!--end #menubar-->
+                <!-- END MENUBAR -->
+
+			</div><!--end .offcanvas-->
+			<!-- END OFFCANVAS LEFT -->
+
+			<!-- BEGIN CONTENT-->
+			<div id="content">
+                
+			</div><!--end #content-->
+			<!-- END CONTENT -->
+
+			<!-- BEGIN OFFCANVAS RIGHT -->
+			<div class="offcanvas">
+
+			</div><!--end .offcanvas-->
+			<!-- END OFFCANVAS RIGHT -->
+
+		</div><!--end #base-->
+		<!-- END BASE -->
+
+		<!-- BEGIN JAVASCRIPT -->
+		<script src="../assets/js/libs/jquery/jquery-1.11.2.min.js"></script>
+		<script src="../assets/js/libs/jquery/jquery-migrate-1.2.1.min.js"></script>
+		<script src="../assets/js/libs/bootstrap/bootstrap.min.js"></script>
+		<script src="../assets/js/libs/spin.js/spin.min.js"></script>
+		<script src="../assets/js/libs/autosize/jquery.autosize.min.js"></script>
+		<script src="../assets/js/libs/nanoscroller/jquery.nanoscroller.min.js"></script>
+		<script src="../assets/js/core/source/App.js"></script>
+		<script src="../assets/js/core/source/AppNavigation.js"></script>
+		<script src="../assets/js/core/source/AppOffcanvas.js"></script>
+		<script src="../assets/js/core/source/AppCard.js"></script>
+		<script src="../assets/js/core/source/AppForm.js"></script>
+		<script src="../assets/js/core/source/AppNavSearch.js"></script>
+		<script src="../assets/js/core/source/AppVendor.js"></script>
+		<script src="../assets/js/core/demo/Demo.js"></script>
+		<!-- END JAVASCRIPT -->
+
+	</body>
+</html>
